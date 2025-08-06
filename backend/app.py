7421a28fff0ec31a6795bbb87bb9043b18167b8f
frasfras <65541080+frasfras@users.noreply.github.com>
2025-08-06 17:10:13 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -15,12 +15,25 @@ def extract_table_from_pdf(pdf_path):
         im = p0.to_image()
         im
         data = []
-        explicit_vertical_lines=[34,62,198,220,370,439,508,579]
+        # bank check
+        month_crop = p0.within_bbox((0, 82, p0.width, 92))
+        
+        bank_chars = month_crop.extract_text()
+        
+        if bank_chars == 'EXPRESS TELLER CHECKING -PHP':
+            explicit_vertical_lines = [34,62,198,220,370,439,508,579]
         for page in pages:
-            table = page.extract_table(table_settings={"vertical_strategy": "explicit",
+            # added 8/02
+            if bank_chars == 'EXPRESS TELLER CHECKING -PHP':
+                explicit_vertical_lines = [34,62,198,220,370,439,508,579]
+                table = page.extract_table(table_settings={"vertical_strategy": "explicit",
                                                        "explicit_vertical_lines": explicit_vertical_lines,
-                                               "horizontal_strategy": "text", 
+                                               "horizontal_strategy": "text",
                                                "snap_tolerance": 4,})[5:]
+            else:
+                table = page.extract_table(table_settings={"vertical_strategy": "lines", 
+                                               "horizontal_strategy": "text", 
+                                               "snap_tolerance": 4,})[0:]
             if table:
                 data.extend(table)
     return data
